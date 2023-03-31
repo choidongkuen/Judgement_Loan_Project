8fc8a522a8f79ec68d75ee06c310d46a5e0879d0
choidongkuen <danaver12@daum.net>
2023-03-31 16:15:02 +0900

    Feat : Api 모듈 build.gradle.kts 구현

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,4 +1,9 @@
 plugins {}
 
 version = "0.0.1"
-dependencies {}
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
+
+    implementation(project(":domain")) // domain gradle.kts 전부 받아서 사용
+}
