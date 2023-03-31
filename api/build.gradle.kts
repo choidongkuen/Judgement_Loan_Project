f76da3538739c9329fca3ca394bf150640bb6f30
choidongkuen <danaver12@daum.net>
2023-04-01 02:59:00 +0900

    Feat : Swagger dependency 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,6 +4,7 @@ version = "0.0.1"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
 
     implementation(project(":domain")) // domain gradle.kts 전부 받아서 사용
 }
