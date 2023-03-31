dd8a91602f8c32efe5546e13b9c4fcf6e4409de2
choidongkuen <danaver12@daum.net>
2023-04-01 01:27:14 +0900

    Feat : LoanRequestDto 클래스 생성
    대출 심사 신청을 위한 응답,요청 dto 클래스

---
+++ api/src/main/kotlin/com/example/api/loan/request/LoanRequestDto.kt
@@ -0,0 +1,23 @@
+package com.example.api.loan.request
+
+
+/*
+request :
+    1. userName, -> 이름
+    2. userIncomeAmount, -> 소득
+    3. userRegistrationNumber -> 주민 번호
+*/
+
+class LoanRequestDto {
+
+    data class LoanRequestInputDto( // 요청
+            val userName: String,
+            val userIncomeAmount: Long,
+            val userRegistrationNumber: String
+    )
+
+
+    data class LoanRequestResponseDto( // 응답
+            val userKey: String
+    )
+}
