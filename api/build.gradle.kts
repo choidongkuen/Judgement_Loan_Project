7f01f15f4aa8dd365a0bea3923b33ee5bdea6735
choidongkuen <danaver12@daum.net>
2023-04-01 19:15:36 +0900

    Feat : AOP 관련 라이브러리 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {}
 
 version = "0.0.1"
 dependencies {
+
+    // spring-boot-starter-web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
@@ -9,7 +11,11 @@ dependencies {
 
     implementation(project(":domain")) // domain gradle.kts 전부 받아서 사용
 
+    // test
     testImplementation("io.mockk:mockk:1.12.0")
     runtimeOnly("com.h2database:h2") // test 사용할 데이터베이스
+
+    // AOP
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 
 }
