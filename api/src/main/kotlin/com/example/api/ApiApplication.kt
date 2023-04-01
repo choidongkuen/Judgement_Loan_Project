a15523b5938c76da63aa0076d26212d2a1a3fc36
choidongkuen <danaver12@daum.net>
2023-04-01 15:57:17 +0900

    Refactor : Main Jpa 관련 어노테이션 제거

--- api/src/main/kotlin/com/example/api/ApiApplication.kt
+++ api/src/main/kotlin/com/example/api/ApiApplication.kt
@@ -3,11 +3,7 @@ package com.example.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@EnableJpaAuditing
-@EnableJpaRepositories(basePackages = ["com.example.domain"])
 @EntityScan(basePackages = ["com.example.domain"])
 @SpringBootApplication
 class ApiApplication
