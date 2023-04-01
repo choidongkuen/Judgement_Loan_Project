c8d7e30119cf806a259a8fa4384b5955a6b463d7
choidongkuen <danaver12@daum.net>
2023-04-01 15:56:03 +0900

    Feat : 직렬화 관련 Jackson 라이브러리 의존성 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -5,9 +5,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
     implementation("io.springfox:springfox-boot-starter:3.0.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.+")
 
     implementation(project(":domain")) // domain gradle.kts 전부 받아서 사용
 
     testImplementation("io.mockk:mockk:1.12.0")
     runtimeOnly("com.h2database:h2") // test 사용할 데이터베이스
+
 }
