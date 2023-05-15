167d3ba045e396ce311051bd64ae8862d526f047
choidongkuen <danaver12@daum.net>
2023-05-15 23:59:10 +0900

    Feat : nginx 이미지 파일 생성 및 container 생성
    - css 모듈로 들어오는 요청에 대한 proxy server 구축을 위한 nginx 서버 기본 설정 구현

--- consumer/src/main/kotlin/kafka/LoanRequestConsumer.kt
+++ consumer/src/main/kotlin/kafka/LoanRequestConsumer.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
+
 @Service
 class LoanRequestConsumer(
     private val objectMapper: ObjectMapper,
