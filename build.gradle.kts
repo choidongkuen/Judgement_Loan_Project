a16d4b09ced6efad11f37273de3d8acd6e637981
choidongkuen <danaver12@daum.net>
2023-05-16 23:26:32 +0900

    Feat : docker-build.sh 을 통한 통합 이미지 생성용 shell script 구현 및 이미지 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ subprojects {
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = "11" // jdk 11
         }
     }
 
---
+++ docker/docker-build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# setting version
+VERSION='1.0.0'
+
+cd ..
+./gradlew clean build -x test
+
+ROOT_PATH=`pwd`
+echo $ROOT_PATH # 위치 잘 찍히는지 test
+
+# api application
+echo 'api docker image build...'
+cd $ROOT_PATH/api && docker build -t api:$VERSION -f ./DockerFile .
+#echo $ROOT_PATH/api
+echo 'api docker image build... Done'
+
+
+echo 'consumer docker image build...'
+cd $ROOT_PATH/consumer && docker build -t consumer:$VERSION -f ./DockerFile .
+
+
+echo 'css docker image build...'
+cd $ROOT_PATH/css && docker build -t css:$VERSION -f ./DockerFile .
+echo 'css docker image build... Done'
+
+
+echo 'nginx docker image build...'
+cd $ROOT_PATH/nginx && docker build -t nginx:$VERSION -f ./DockerFile .
+echo 'nginx docker image build... Done'
