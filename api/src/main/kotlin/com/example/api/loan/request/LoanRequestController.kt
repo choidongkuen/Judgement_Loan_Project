f2b40ac9e7b53cd3fdbfdf1416af5256a47b3bf3
choidongkuen <danaver12@daum.net>
2023-05-29 21:34:37 +0900

    Feat : controller swagger 관련 추가 설정

--- api/src/main/kotlin/com/example/api/loan/request/LoanRequestController.kt
+++ api/src/main/kotlin/com/example/api/loan/request/LoanRequestController.kt
@@ -1,5 +1,7 @@
 package com.example.api.loan.request
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -7,18 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 // 대출 심사 요청하는 클래스
+@Api(tags = ["대출 심사 요청 API"])
 @RestController
 @RequestMapping("/loan/api/v1")
 class LoanRequestController(
-        private val loanRequestServiceImpl: LoanRequestServiceImpl
+    private val loanRequestServiceImpl: LoanRequestServiceImpl
 ) {
 
+    @ApiOperation(value = "회원 정보 저장 및 대출 심사 요청하는 API")
     @PostMapping("/request")
     fun loanRequest(
-            @RequestBody loanRequestInputDto: LoanRequestDto.LoanRequestInputDto
+        @RequestBody loanRequestInputDto: LoanRequestDto.LoanRequestInputDto
     ): ResponseEntity<LoanRequestDto.LoanRequestResponseDto> {
         return ResponseEntity.ok(
-                loanRequestServiceImpl.loanRequestMain(loanRequestInputDto)
+            loanRequestServiceImpl.loanRequestMain(loanRequestInputDto)
         )
     }
 }
--- api/src/main/kotlin/com/example/api/loan/review/LoanReviewController.kt
+++ api/src/main/kotlin/com/example/api/loan/review/LoanReviewController.kt
@@ -1,5 +1,7 @@
 package com.example.api.loan.review
 
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -8,18 +10,20 @@ import org.springframework.web.bind.annotation.RestController
 
 
 // 대출 심사 결과 요청하는 컨트롤러
+@Api(tags = ["대출 심사 결과 요청 API"])
 @RestController
 @RequestMapping("/loan/api/v1")
 class LoanReviewController(
-        private val loanReviewServiceImpl: LoanReviewServiceImpl
+    private val loanReviewServiceImpl: LoanReviewServiceImpl
 ) {
 
+    @ApiOperation(value = "대출 심사 결과 요청하는 API")
     @GetMapping("/review/{userKey}")
     fun getReviewResult(
-            @PathVariable userKey: String
+        @PathVariable userKey: String
     ): ResponseEntity<LoanReviewDto.LoanReviewResponseDto> {
         return ResponseEntity.ok(
-                loanReviewServiceImpl.loanReviewMain(userKey)
+            loanReviewServiceImpl.loanReviewMain(userKey)
         )
     }
 }
